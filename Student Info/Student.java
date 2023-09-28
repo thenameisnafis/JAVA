82632749bb0675d8c70527f60c856dfb4a7431d8
Nafis Hasan <118046574+thenameisnafis@users.noreply.github.com>
2023-09-28 15:37:11 +0600

    Add files via upload

---
+++ Student Info/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private String name;
+    private int id;
+    private double cgpa;
+
+    
+    public Student(String name, int id, double cgpa) {
+        this.name = name;
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getCGPA() {
+        return cgpa;
+    }
+
+    public static void main(String[] args) {
+        
+        Student student = new Student("Nafis Hasan", 50976, 3.00);
+
+        
+        System.out.println("Name: " + student.getName());
+        System.out.println("ID: " + student.getId());
+        System.out.println("CGPA: " + student.getCGPA());
+    }
+}
---
+++ Student Info/StudentInfo.java
@@ -0,0 +1,31 @@
+public class Student {
+    private String name;
+    private int id;
+    private double cgpa;
+
+    public Student(String name, int id, double cgpa) {
+        this.name = name;
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getCGPA() {
+        return cgpa;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Nafis Hasan", 12345, 3.75);
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("ID: " + student.getId());
+        System.out.println("CGPA: " + student.getCGPA());
+    }
+}
